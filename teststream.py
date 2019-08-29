c1cbe51d21733ca8e8b38b2439a9ee2c05b328cb
imtimwong <20898842+imtimwong@users.noreply.github.com>
2019-08-30 00:29:20 +0800

    seperated into different classes and differentiated between RT status. Plus printing out the tweets and pumping it into a file

--- teststream.py
+++ teststream.py
@@ -1,63 +1,119 @@
-#! /usr/bin/python3.7
+#! /usr/local/bin/python3
 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-#import tweepy
- 
+from tweepy import API
+import json
+
 import credentialstwitter
- 
-# # # # TWITTER STREAMER # # # #
-# class TwitterStreamer():
-#     """
-#     Class for streaming and processing live tweets.
-#     """
-#     def __init__(self):
-#         pass
-
-#     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
-#         # This handles Twitter authetification and the connection to Twitter Streaming API
-#         listener = StdOutListener(fetched_tweets_filename)
-#         auth = OAuthHandler(credentialstwitter.CONSUMER_KEY, credentialstwitter.CONSUMER_SECRET)
-#         auth.set_access_token(credentialstwitter.ACCESS_TOKEN, credentialstwitter.ACCESS_TOKEN_SECRET)
-#         stream = Stream(auth, listener)
-
-#         # This line filter Twitter Streams to capture data by the keywords: 
-#         stream.filter(track=hash_tag_list)
 
 
-# # # # TWITTER STREAM LISTENER # # # #
-class StdOutListener(StreamListener):
+# # # Streamer
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets.
+    """
+    #def __init__(self):
+
+
+    def stream_tweets(self, fetched_tweets_filename, topics):
+
+        listener = thestreamListener(fetched_tweets_filename)
+        streamit = Stream(auth, listener)
+
+        streamit.filter(track=topics)
+
+
+# # # # Stream listener
+class thestreamListener(StreamListener):
     """
     This is a basic listener that just prints received tweets to stdout.
     """
-    # def __init__(self, fetched_tweets_filename):
-    #     self.fetched_tweets_filename = fetched_tweets_filename
 
-    # def on_data(self, data):
-    #     try:
-    #         print(data)
-    #         with open(self.fetched_tweets_filename, 'a') as tf:
-    #             tf.write(data)
-    #         return True
-    #     except BaseException as e:
-    #         print("Error on_data %s" % str(e))
-    #     return True
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
 
-    def on_data(self, data):
-    	print(data)
-    	return True
-          
+    def on_data(self, ori_raw_data):
+
+        try:
+
+            data = json.loads(ori_raw_data)
+
+            # Obtain all the variables to store in each column
+            #tweet_topics=topics[0],topics[1]
+            tweet_topics=topics
+            user_id = data['user']['id']
+            user_name = data['user']['name']
+            twitter_user_name = data['user']['screen_name']
+            created_at = data['created_at']
+            tweet = data['text']
+
+            #print(data)
+            #if 'extended_tweet' in data and 'RT @' not in data['extended_tweet']['full_text'].encode('utf-8'):
+            if 'retweeted_status' in data :#and 'RT @' not in data.extended_tweet.['full_text']:
+
+
+                #print("its a retweet",twitter_user_name)
+                retweetstatus_user = data['retweeted_status']['user']['id']
+                retweetstatus_name = data['retweeted_status']['user']['name']
+                #print(data)
+                #print(data['extended_tweet'])
+
+
+            else:
+                #print("its not a retweet")
+                #print(data['text'])
+                #print(data['extended_tweet']['full_text'])
+                retweetstatus_user = ""
+                retweetstatus_name = ""
+
+            print(tweet_topics,"§",user_id,"§",twitter_user_name,"§",user_name,"§",tweet,"§",retweetstatus_user
+                  ,"§",retweetstatus_name,"§",created_at,)
+            datajson=tweet_topics,user_id,twitter_user_name,user_name,tweet,retweetstatus_user,retweetstatus_name,created_at
+            #print(datajson)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(str(tweet_topics)+"§"+str(user_id)+"§"+str(twitter_user_name)+"§"+str(user_name)+"§"+str(tweet)+"§"+str(retweetstatus_user)+"§"+str(retweetstatus_name)+"§"+str(created_at)+"\n")
+            return True
+
+            # Store them in the corresponding table in the database
+            # load_into_db(topics, user_id, twitter_user_name, user_name, tweet, retweetstatus_user,
+            #                       retweetstatus_name, created_at)
+
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+
+
+
 
     def on_error(self, status):
         print(status)
 
- 
-if __name__ == '__main__':
- 
-   listener = StdOutListener()
-   auth = OAuthHandler(credentialstwitter.CONSUMER_KEY, credentialstwitter.CONSUMER_SECRET)
-   auth.set_access_token(credentialstwitter.ACCESS_TOKEN, credentialstwitter.ACCESS_TOKEN_SECRET)
-   stream = Stream(auth, listener)
 
-   stream.filter(track=['donald trump'])
+class authenticateTwitterClass():
+    """
+	reads from credentialstwitter file that contains the keys and access tokens
+	"""
+
+    def authenticateTwitter(self):
+        auth = OAuthHandler(credentialstwitter.CONSUMER_KEY, credentialstwitter.CONSUMER_SECRET)
+        auth.set_access_token(credentialstwitter.ACCESS_TOKEN, credentialstwitter.ACCESS_TOKEN_SECRET)
+
+        return auth
+
+if __name__ == '__main__':
+
+    topics = ["Taylor Swift", "Lover"]
+    fetched_tweets_filename = "tweets.txt"
+
+    api = API(wait_on_rate_limit_notify=True)
+
+    auth = authenticateTwitterClass().authenticateTwitter()
+
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, topics)
+
+
+
+
