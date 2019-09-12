b04b67e45d2ae8549c32f90f8601ca7a9d2f67b7
imtimwong <20898842+imtimwong@users.noreply.github.com>
2019-09-12 19:53:36 +0800

    adding preprocesing feature

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -11,6 +11,8 @@ import sqlalchemy as db
 import numpy as np
 import pandas as pd
 
+import re
+
 
 
 sqlengine = db.create_engine('postgres+psycopg2://%s:%s@%s:%s/%s'%(postgrescredentials.user,postgrescredentials.password,postgrescredentials.host,postgrescredentials.port,postgrescredentials.database))
@@ -26,7 +28,49 @@ s="SELECT * FROM TWEETS;"
 
 df = pd.read_sql(s,con, index_col="tweet_id")
 
-print(df.head(10))
+#put everythin in lowercase
+df['tweet_text'] = df['tweet_text'].str.lower()
+#Replace rt indicating that was a retweet_text
+df['tweet_text'] = df['tweet_text'].str.replace('rt', '')
+#Replace occurences of mentioning @UserNames
+df['tweet_text'] = df['tweet_text'].replace(r'@\w+', '', regex=True)
+#Replace links contained in the tweet_text
+df['tweet_text'] = df['tweet_text'].replace(r'http\S+', '', regex=True)
+df['tweet_text'] = df['tweet_text'].replace(r'www.[^ ]+', '', regex=True)
+#remove next line \n
+df['tweet_text'] = df['tweet_text'].replace('\n','', regex=True)
+#remove numbers
+df['tweet_text'] = df['tweet_text'].replace(r'[0-9]+', '', regex=True)
+#replace special characters and puntuation marks
+df['tweet_text'] = df['tweet_text'].replace(r'[!"#$%&()*+,-./:;<=>?@[\]^_`{|}~]', '', regex=True)
+
+
+
+#print(df.head(10))
+#print(df.head(10))
+#print(df['tweet_text'].str.replace('RT', ''))
+
+# cleanedtweet = str(df['tweet_text'])
+#
+# cleanedtweet = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", cleanedtweet).split())
+#
+# #aiya just copy fucj
+# #def clean_tweet(self, cleaned=df['tweet_text']):
+# #    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", df['tweet_text']).split())
+#
+# #print(df['tweet_text'])
+# print(cleanedtweet)
+# df['tweet_text']=cleanedtweet
+#
+print(df['tweet_text'].iloc[2])
+
+# for row in df:
+#     cleanedtweet = str(df['tweet_text'])
+# 
+#     cleanedtweet = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", cleanedtweet).split())
+#     df['tweet_text'] = cleanedtweet
+# 
+#     print(df['tweet_text'])
 
 # for row in result:
 #     print(row[tweets.c.tweet_text])
