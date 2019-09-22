e501413c4421d2023da5e9d0b1d4dfa7ba8d07e4
imtimwong <20898842+imtimwong@users.noreply.github.com>
2019-09-22 18:51:28 +0800

    generated graph and updated features

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -268,43 +268,7 @@ class wordfreq():
         plt.savefig(graphpath, format="png")
         #plt.show()
 
-    # # Vectorization for Data Visualization
-    # def vectorization(self,df):
-    #     # CountVectorizer will convert a collection of text documents to a matrix of token counts
-    #     # Produces a sparse representation of the counts
-    #     # Initialize
-    #     vector = CountVectorizer()
-    #     # We fit and transform the vector created
-    #     frequency_matrix = vector.fit_transform(df.tweet_text)
-    #     # Sum all the frequencies for each word
-    #     sum_frequencies = np.sum(frequency_matrix, axis=0)
-    #     # Now we use squeeze to remove single-dimensional entries from the shape of an array that we got from applying np.asarray to
-    #     # the sum of frequencies.
-    #     frequency = np.squeeze(np.asarray(sum_frequencies))
-    #     # Now we get into a dataframe all the frequencies and the words that they correspond to
-    #     frequency_df = pd.DataFrame([frequency], columns=vector.get_feature_names()).transpose()
-    #
-    #     print(df.tweet_text)
-    #     print(frequency_df)
-    #     return frequency_df
-    #
-    # def graph(self,word_frequency, sent):
-    #     labels = word_frequency[0][1:51].index
-    #     title = "Word Frequency for %s" % sent
-    #     # Plot the figures
-    #     plt.figure(figsize=(10, 5))
-    #     plt.bar(np.arange(50), word_frequency[0][1:51], width=0.8, color=sns.color_palette("bwr"), alpha=0.5,
-    #             edgecolor="black", capsize=8, linewidth=1);
-    #     plt.xticks(np.arange(50), labels, rotation=90, size=14);
-    #     plt.xlabel("50 more frequent words", size=14);
-    #     plt.ylabel("Frequency", size=14);
-    #     # plt.title('Word Frequency for %s', size=18) %sent;
-    #     plt.title(title, size=18)
-    #     plt.grid(False);
-    #     plt.gca().spines["top"].set_visible(False);
-    #     plt.gca().spines["right"].set_visible(False);
-    #     plt.show()
-
+ 
 
 
 #https://towardsdatascience.com/natural-language-processing-count-vectorization-with-scikit-learn-e7804269bb5e
